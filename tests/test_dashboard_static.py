07159945824082b6ae57d95c2483b5431886f41c
Yinglianchun <yinglianchun55123@gmail.com>
2026-05-23 22:12:46 +0800

    Show year ring authors in dashboard

--- tests/test_dashboard_static.py
+++ tests/test_dashboard_static.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 
 
-def test_dashboard_comments_show_time_without_emotion_fields():
+def test_dashboard_comments_show_author_and_time_without_emotion_fields():
     html = Path("dashboard.html").read_text(encoding="utf-8")
 
     assert "var commentTime = c.original_feel_created || c.created || '';" in html
-    assert '<div class="comment-time">' in html
+    assert "let dashboardAiAuthor = 'Haven';" in html
+    assert "function commentAuthorName(comment)" in html
+    assert '<div class="comment-meta">' in html
+    assert '<span class="comment-author">' in html
+    assert '<span class="comment-time">' in html
 
     comments_block = html.split("var commentsHtml = comments.length", 1)[1].split("var commentFormHtml", 1)[0]
+    assert "commentAuthorName(c)" in comments_block
     assert "c.valence" not in comments_block
     assert "c.arousal" not in comments_block
 
